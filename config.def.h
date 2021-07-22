fd1e5591f7dccab70ba501cf4283165a755b6ade
Nicolo Traini <nicolo.traini@oncode.it>
2021-07-23 01:02:41 +0200

    Patch highpriority

--- config.def.h
+++ config.def.h
@@ -17,6 +17,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeHp] = { "#bbbbbb", "#333333" }
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
